a45d4a2faccc8a838431ec175913e006db5f89c5
Adelin Chis <adelingabriel08@gmail.com>
2022-04-01 13:43:10 +0300

    multicolor-blending setup

--- BouncySquare/app/src/main/java/cg/bouncysquare/BouncySquareActivity.java
+++ BouncySquare/app/src/main/java/cg/bouncysquare/BouncySquareActivity.java
@@ -14,7 +14,7 @@ public class BouncySquareActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         GLSurfaceView view = new GLSurfaceView(this);
-        view.setRenderer(new SquareRenderer(this.getApplicationContext()));
+        view.setRenderer(new SquareRenderer());
         setContentView(view);
     }
 }
--- BouncySquare/app/src/main/java/cg/bouncysquare/SquareRenderer.java
+++ BouncySquare/app/src/main/java/cg/bouncysquare/SquareRenderer.java
@@ -1,6 +1,5 @@
 package cg.bouncysquare;
 
-import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.renderscript.ScriptIntrinsicYuvToRGB;
 
@@ -9,16 +8,27 @@ import javax.microedition.khronos.opengles.GL10;
 import javax.microedition.khronos.opengles.GL11;
 
 public class SquareRenderer implements GLSurfaceView.Renderer{
-    private final Square mSquare1;
-    private final Square mSquare2;
+    private Square mSquare1;
+    private Square mSquare2;
     private float mTransY;
-    private final Context context;
 
-    public SquareRenderer(Context context)
-    {
-        this.context = context;
-        this.mSquare1 = new Square();
-        this.mSquare2 = new Square();
+    public SquareRenderer() {
+        float[] squareColorsYMCA =
+                {
+                        1.0f, 1.0f, 0.0f, 1.0f,
+                        0.0f, 1.0f, 1.0f, 1.0f,
+                        0.0f, 0.0f, 0.0f, 1.0f,
+                        1.0f, 0.0f, 1.0f, 1.0f
+                };
+        float[] squareColorsRGBA =
+                {
+                        1.0f, 0.0f, 0.0f, 1.0f,
+                        0.0f, 1.0f, 0.0f, 1.0f,
+                        0.0f, 0.0f, 1.0f, 1.0f,
+                        1.0f, 1.0f, 1.0f, 1.0f
+                };
+        mSquare1 = new Square(squareColorsYMCA);
+        mSquare2 = new Square(squareColorsRGBA);
     }
 
     @Override
@@ -29,8 +39,6 @@ public class SquareRenderer implements GLSurfaceView.Renderer{
         gl.glEnable(GL10.GL_CULL_FACE);
         gl.glShadeModel(GL10.GL_SMOOTH);
         gl.glEnable(GL10.GL_DEPTH_TEST);
-//        int resid = R.drawable.dog;
-//        mSquare.createTexture(gl, this.context, resid);
     }
 
     @Override
@@ -48,17 +56,22 @@ public class SquareRenderer implements GLSurfaceView.Renderer{
         gl.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         gl.glMatrixMode(GL11.GL_MODELVIEW);
         gl.glEnableClientState(GL11.GL_VERTEX_ARRAY);
-
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
         gl.glLoadIdentity();
         gl.glTranslatef(0.0f,(float)Math.sin(mTransY), -3.0f);
-        gl.glColor4f(0.0f,1.0f,1.0f,1.0f);
-        gl.glEnable(GL10.GL_BLEND);
+//        gl.glColor4f(0.0f,0.0f,1.0f,0.5f);
+//        gl.glEnable(GL10.GL_BLEND);
+////        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+//        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+//        gl.glColorMask(true, false, true, true);
         mSquare1.draw(gl);
 
         gl.glLoadIdentity();
         gl.glTranslatef((float)(Math.sin(mTransY)/2.0f),0.0f, -2.9f);
-        gl.glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+//        gl.glColor4f(1.0f, 0.0f, 0.0f, 0.5f);
         mSquare2.draw(gl);
         mTransY += .075f;
     }
+
+
 }
